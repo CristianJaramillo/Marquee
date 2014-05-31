34d6585c33765608be02e2ea02f9de57df6b02a1
By Cristian Jaramillo <cristianjaramillo32@gmail.com>
2014-05-30 23:58:54 -0500

    Projet Calif

---
+++ Marquee_CristianJaramillo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Marquee_CristianJaramillo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Salir = new System.Windows.Forms.Button();
+            this.Terminar = new System.Windows.Forms.Button();
+            this.Reanudar = new System.Windows.Forms.Button();
+            this.Pausa = new System.Windows.Forms.Button();
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Salir
+            // 
+            resources.ApplyResources(this.Salir, "Salir");
+            this.Salir.Name = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Terminar
+            // 
+            resources.ApplyResources(this.Terminar, "Terminar");
+            this.Terminar.Name = "Terminar";
+            this.Terminar.UseVisualStyleBackColor = true;
+            this.Terminar.Click += new System.EventHandler(this.Terminar_Click);
+            // 
+            // Reanudar
+            // 
+            resources.ApplyResources(this.Reanudar, "Reanudar");
+            this.Reanudar.Name = "Reanudar";
+            this.Reanudar.UseVisualStyleBackColor = true;
+            this.Reanudar.Click += new System.EventHandler(this.Reanudar_Click);
+            // 
+            // Pausa
+            // 
+            resources.ApplyResources(this.Pausa, "Pausa");
+            this.Pausa.Name = "Pausa";
+            this.Pausa.UseVisualStyleBackColor = true;
+            this.Pausa.Click += new System.EventHandler(this.Pausa_Click);
+            // 
+            // Iniciar
+            // 
+            resources.ApplyResources(this.Iniciar, "Iniciar");
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Terminar);
+            this.Controls.Add(this.Reanudar);
+            this.Controls.Add(this.Pausa);
+            this.Controls.Add(this.Iniciar);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Terminar;
+        private System.Windows.Forms.Button Reanudar;
+        private System.Windows.Forms.Button Pausa;
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Marquee_CristianJaramillo/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Marquee_CristianJaramillo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        string cad1, aux, cola;
+        Thread Hebra;
+        public void Hilo()
+        {
+            while (true)
+            {
+                Thread.Sleep(5);
+                if (i < 240)
+                    i += 10;
+
+                else
+                {
+                    i = 0;
+                    for (i = 0; i < 1; i++)
+                    {
+                        cad1 = textBox1.Text;
+                        cola = cad1.Substring(0, 1);
+                        aux = cad1.Substring(1, cad1.Length - 1);
+                        textBox1.Text = aux.ToString() + cola.ToString();
+                    }
+                }
+            }
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " Hola soy el número uno";
+            Hebra = new Thread(new ThreadStart(Hilo));
+            Hebra.Start();
+            Iniciar.Enabled = false;
+            Pausa.Enabled = true;
+            Reanudar.Enabled = false;
+        }
+
+        private void Pausa_Click(object sender, EventArgs e)
+        {
+            Hebra.Suspend();
+            Reanudar.Enabled = true;
+            Terminar.Enabled = false;
+            i = 2;
+        }
+
+        private void Reanudar_Click(object sender, EventArgs e)
+        {
+            Hebra.Resume();
+            Reanudar.Enabled = false;
+            Terminar.Enabled = true;
+            Pausa.Enabled = true;
+        }
+
+        private void Terminar_Click(object sender, EventArgs e)
+        {
+            if (i == 0)
+                MessageBox.Show("No hay proceso que terminar!!");
+
+            else
+                if (Hebra.IsAlive)
+                {
+                    //// condicion
+                    //i = 0;
+                    Iniciar.Enabled = true;
+                    Pausa.Enabled = false;
+                    Reanudar.Enabled = false;
+                    Hebra.Abort();
+                    textBox1.Text = "";
+                }
+                else
+                    MessageBox.Show("No hay proceso que finalizar");
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            if (i == 0)
+                Application.Exit();
+            else
+                if (Hebra.IsAlive && i == 2)
+                    MessageBox.Show("Reanude la aplicacion y presione 'Terminar' para que pueda Salir correctamente!");
+                else
+                    Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
